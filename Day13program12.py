65870ab64b1b30483f4fd5fd800dae6a81b32bb4
Devilakshm <121248953+Devilakshm@users.noreply.github.com>
2023-01-04 16:56:45 +0530

    Add files via upload

---
+++ Day13program12.py
@@ -0,0 +1,11 @@
+##fo= open(r'C:\pythonpractice(29)\Day13\Lucky1.txt')
+##temp=fo.read()
+##print(temp)
+##fo.close()
+
+
+with open(r'C:\pythonpractice(29)\Day13\Lucky1.txt')as fo:
+     temp=fo.read()
+     print(temp)
+
+     
